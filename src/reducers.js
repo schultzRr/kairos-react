7267e98435834387518bbc87417c8c713f0388a4
schultzRr <rosas_schultz@hotmail.com>
2020-04-24 17:51:15 -0500

    Updated package-lock.json

--- src/reducers.js
+++ src/reducers.js
@@ -1,9 +1,7 @@
 import navigation from 'components/navigation/navigationReducer';
 import loginView from 'views/login/loginViewReducer';
 import login from 'components/login/loginReducer';
-import registerView from 'views/register/registerViewReducer';
 import register from 'components/register/registerReducer';
-import forgotView from 'views/forgot/forgotViewReducer';
 import forgot from 'components/forgot/forgotReducer';
 import session from 'http/sessionReducer';
 import dashboard from 'components/dashboard/dashboardReducer';
@@ -16,9 +14,7 @@ const reducers = combineReducers({
   navigation,
   loginView,
   login,
-  registerView,
   register,
-  forgotView,
   forgot,
   session,
   dashboard,
